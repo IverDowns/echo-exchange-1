21eb33c6b528449b002ab7f086c14448cefa5a63
SamChav07 <schavarriab@uamv.edu.ni>
2023-10-28 22:38:33 -0600

    Base del proyecto

---
+++ data.h
@@ -0,0 +1,51 @@
+#ifndef DATA_H
+#define DATA_H
+
+#define MAX 100
+
+struct accounts {
+    struct client;
+    int admin_id = 00017;
+    const char* adm_pass = "admPass007";
+}acsts;
+
+
+struct cliente {
+    char client_name[20];
+    char client_pass;
+    int client_id;
+    char client_mail;
+    char client_telf;
+}clt;
+ 
+
+struct reg_compra {
+    int cmpr_id;
+    struct cmpr_fecha;
+    float cmpr_qty; // monto de la compra
+    int cmpr_pts;
+}r_cmpr;
+
+
+struct cmpr_fecha {
+    int day;
+    int month;
+    int year;
+}cmpr_f;
+
+
+struct historial {
+    struct client; 
+    struct reg_compra;
+    int cmpr_ptsTot;
+}hst;
+
+
+struct gift {
+    int gft_cant;
+    char gft_name;
+    int gft_pts;
+}gft;
+ 
+#endif
+
